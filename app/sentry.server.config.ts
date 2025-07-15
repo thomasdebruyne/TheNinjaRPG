71924899bdc7d2abdcb08217476ac9219685a870
Mathias Gruber <nano.mathias@gmail.com>
2025-07-15 16:39:28 +0200

    :loud_sound: Add debug on logging from backend

--- app/sentry.server.config.ts
+++ app/sentry.server.config.ts
@@ -17,7 +17,7 @@ Sentry.init({
   ignoreErrors: ["Unauthorized for tRPC endpoint"],
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+  debug: true,
 
   // Adds request headers and IP for users, for more info visit:
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
