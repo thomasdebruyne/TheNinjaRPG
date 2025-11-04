fcfdf44b82009944c9be27aeea45afe0522be052
Phrosfire <65364337+Phrosfire@users.noreply.github.com>
2025-11-03 23:16:49 -0500

    Remove unnecessary query
    Signed-off-by: Phrosfire <65364337+Phrosfire@users.noreply.github.com>

--- app/drizzle/migrations/0304_confused_serpent_society.sql
+++ app/drizzle/migrations/0304_confused_serpent_society.sql
@@ -1,3 +1,2 @@
-ALTER TABLE `Item` MODIFY COLUMN `crystalTargetTypes` enum('WEAPON','CONSUMABLE','ARMOR','ACCESSORY','MATERIAL','KEYSTONE','CRYSTAL','OTHER');
 ALTER TABLE `AuctionListing` ADD `currencyType` enum('MONEY','REPUTATION') DEFAULT 'MONEY' NOT NULL;
 ALTER TABLE `UserRequest` ADD `spectatable` boolean DEFAULT false;
